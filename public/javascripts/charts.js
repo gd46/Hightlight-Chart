565aa6766a85433a2f74497fc219d7ff6f4a897e
Giuseppe DiBella <gd46@njit.edu>
2015-04-09 23:19:05 -0400

    Added example chart

--- public/javascripts/charts.js
+++ public/javascripts/charts.js
@@ -1,4 +1,53 @@
 $(document).ready(function() {
 
-	console.log("test");
+	var test = {
+			data: [{
+	            studentName: 'John',
+	            data: [5, 3, 4]
+	        }, {
+	            studentName: 'Jane',
+	            data: [2, 2, 3]
+	        }, {
+	            studentName: 'Joe',
+	            data: [3, 4, 4]
+	        }]
+		};
+		series = [],
+		len = test.data.length,
+		i = 0;
+	for(i; i<len;i++){
+		console.log(i);
+		series.push({
+			name: test.data[i].studentName,
+			data: test.data[i].data
+		});
+	}
+		$('#container').highcharts({
+		    chart: {
+		        type: 'bar'
+		    },
+		    title: {
+		        text: 'Students'
+		    },
+		    xAxis: {
+		        categories: ['Apples', "Oranges", "Pairs"]
+		    },
+		    yAxis: {
+		        title: {
+		            text: 'Number of students'
+		        }
+		    },
+		    series: series
+
+		    //test.data
+		    // series: [{
+		    //     name: 'Males',
+		    //     //data: [test.students.male]
+		    //     data: [1200]
+		    // }, {
+		    //     name: 'Females',
+		    //     //data: [test.students.female]
+		    //     data: [500]
+		    // }]
+		});
 });
